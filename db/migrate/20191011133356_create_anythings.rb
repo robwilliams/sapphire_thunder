615972a25deeade39cff06b8abdf429e71890f80
Robert Williams <rob@r-williams.com>
2019-10-11 15:08:30 +0100

    Rubocop

--- db/migrate/20191011133356_create_anythings.rb
+++ db/migrate/20191011133356_create_anythings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateAnythings < ActiveRecord::Migration[6.0]
   def change
     create_table :anythings do |t|
--- test/models/anything_test.rb
+++ test/models/anything_test.rb
@@ -1,4 +1,6 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class AnythingTest < ActiveSupport::TestCase
   # test "the truth" do
