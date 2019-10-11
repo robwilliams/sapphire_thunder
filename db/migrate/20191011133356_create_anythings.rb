434f4d66400936c672246aa364bdbbe5bf136ca0
Robert Williams <rob@r-williams.com>
2019-10-11 14:34:10 +0100

    Add any old model

---
+++ db/migrate/20191011133356_create_anythings.rb
@@ -0,0 +1,9 @@
+class CreateAnythings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :anythings do |t|
+      t.string :something
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/anything_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnythingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
